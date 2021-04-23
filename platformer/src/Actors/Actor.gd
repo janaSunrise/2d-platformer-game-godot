f83e4dec882810d0264a7b9579f5f94dd9a350ed
janaSunrise <warriordefenderz@gmail.com>
2021-04-23 23:49:59 +0530

    Modify the logic using speed variable.

--- platformer/src/Actors/Actor.gd
+++ platformer/src/Actors/Actor.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 class_name Actor
 
+export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	move_and_slide(velocity)
+	velocity.y += gravity * delta	
+	velocity = move_and_slide(velocity)
