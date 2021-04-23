882c5adadf2830184025b9ac538082194f30a3e0
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 00:31:58 +0530

    Added input mapping, and Basic left and right movement physics.

--- platformer/src/Actors/Player.gd
+++ platformer/src/Actors/Player.gd
@@ -1,2 +1,9 @@
 extends Actor
 
+func _physics_process(delta: float) -> void:
+	var direction: = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		1.0
+	)
+	velocity = speed * direction
+	velocity = move_and_slide(velocity)
