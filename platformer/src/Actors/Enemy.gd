447f38f054a706ad1d4978f91d0521a3865ac615
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 15:12:33 +0530

    Allow stomping and killing the enemy.

--- platformer/src/Actors/Enemy.gd
+++ platformer/src/Actors/Enemy.gd
@@ -6,6 +6,14 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 
 
+func _on_StompDetector_body_entered(body: Node) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	
--- platformer/src/Actors/Player.gd
+++ platformer/src/Actors/Player.gd
@@ -1,5 +1,13 @@
 extends Actor
 
+
+export var stomp_impulse: = 1000.0
+
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
@@ -31,5 +39,14 @@ func calculate_move_velocity(
 
 	if is_jump_interrupted:
 		out.y = 0.0
+	
+	return out
+	
+
+func calculate_stomp_velocity(
+	linear_velocity: Vector2, impulse: float
+) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
 	
 	return out
