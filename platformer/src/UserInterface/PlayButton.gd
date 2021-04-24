149ccb32a933f794fe50cd4efa628870e7e09b51
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 23:26:17 +0530

    Completed main screen, Work on End screen.

---
+++ platformer/src/UserInterface/PlayButton.gd
@@ -0,0 +1,10 @@
+tool
+extends Button
+
+export(String, FILE) var next_scene_path: = ""
+
+func _on_button_up() -> void:
+	get_tree().change_scene(next_scene_path)
+	
+func _get_configuration_warning() -> String:
+	return "The property Next Level can't be empty" if next_scene_path == "" else ""
