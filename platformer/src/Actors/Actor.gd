4c08eac05bfad41c9dfebe19445f0c486e109c72
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 12:46:52 +0530

    Get Jumping of the player sprite working, Fix physics logic.

--- platformer/src/Actors/Actor.gd
+++ platformer/src/Actors/Actor.gd
@@ -1,12 +1,9 @@
 extends KinematicBody2D
 class_name Actor
 
+const FLOOR_NORMAL: = Vector2.UP
+
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0 
 
 var velocity: = Vector2.ZERO
-
-
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta	
-	velocity = move_and_slide(velocity)
--- platformer/src/Actors/Player.gd
+++ platformer/src/Actors/Player.gd
@@ -1,9 +1,30 @@
 extends Actor
 
 func _physics_process(delta: float) -> void:
-	var direction: = Vector2(
+	var direction: = get_direction()
+
+	velocity = calculate_move_velocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+
+
+func get_direction() -> Vector2:
+	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		1.0
+		- 1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
-	velocity = speed * direction
-	velocity = move_and_slide(velocity)
+
+
+func calculate_move_velocity(
+	linear_velocity: Vector2,
+	direction: Vector2,
+	speed: Vector2
+) -> Vector2:
+	var new_velocity: = linear_velocity
+	
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	
+	if direction.y == -1.0:
+		new_velocity.y = speed.y * direction.y
+	
+	return new_velocity
