36b5b5a8c9d7f1e28d7223d624b075644c111030
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 14:20:27 +0530

    Worked on basic Enemy movement.

--- platformer/src/Actors/Enemy.gd
+++ platformer/src/Actors/Enemy.gd
@@ -1,2 +1,14 @@
 extends "res://src/Actors/Actor.gd"
 
+
+func _ready() -> void:
+	_velocity.x = -speed.x
+
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
+	
+	if is_on_wall():
+		_velocity.x *= -1.0
+
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
