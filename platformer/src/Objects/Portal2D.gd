8631661751b86f1ab049f8c9745e621b8062f136
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 18:53:56 +0530

    Move objects to new location.

---
+++ platformer/src/Objects/Portal2D.gd
@@ -0,0 +1,21 @@
+tool
+extends Area2D
+
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+export var next_scene: PackedScene
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	teleport()
+
+func _get_configuration_warning() -> String:
+	return "The next scene property cannot be empty" if next_scene else ""
+
+func teleport() -> void:
+	get_tree().paused = true
+	anim_player.play("fade_out")
+	
+	yield(anim_player, "animation_finished")
+	
+	get_tree().paused = false
+	get_tree().change_scene_to(next_scene)
