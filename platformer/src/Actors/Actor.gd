06510a0c1cecb130008b36359c84594e1c38c155
janaSunrise <warriordefenderz@gmail.com>
2021-04-24 13:21:04 +0530

    Rename the variables to keep code maintainable.

--- platformer/src/Actors/Actor.gd
+++ platformer/src/Actors/Actor.gd
@@ -6,4 +6,4 @@ const FLOOR_NORMAL: = Vector2.UP
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0 
 
-var velocity: = Vector2.ZERO
+var _velocity: = Vector2.ZERO
--- platformer/src/Actors/Player.gd
+++ platformer/src/Actors/Player.gd
@@ -1,17 +1,17 @@
 extends Actor
 
 func _physics_process(delta: float) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		- 1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
 
 
@@ -21,15 +21,15 @@ func calculate_move_velocity(
 	speed: Vector2,
 	is_jump_interrupted: bool
 ) -> Vector2:
-	var new_velocity: = linear_velocity
+	var out: = linear_velocity
 	
-	new_velocity.x = speed.x * direction.x
-	new_velocity.y += gravity * get_physics_process_delta_time()
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
 	
 	if direction.y == -1.0:
-		new_velocity.y = speed.y * direction.y
+		out.y = speed.y * direction.y
 
 	if is_jump_interrupted:
-		new_velocity.y = 0.0
+		out.y = 0.0
 	
-	return new_velocity
+	return out
