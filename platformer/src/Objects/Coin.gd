bb71db0e76f93d90867661723652efb4bdf00711
janaSunrise <warriordefenderz@gmail.com>
2021-04-25 13:53:34 +0530

    Get pause interface working, Fixed bugs.

--- platformer/src/Objects/Coin.gd
+++ platformer/src/Objects/Coin.gd
@@ -5,8 +5,8 @@ onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 export var score: = 100
 
 func _on_body_entered(body: Node) -> void:
-	anim_player.play("fade_out")
+	picked()
 	
 func picked() -> void:
 	PlayerData.score += score
-	anim_player.play("picked")
+	anim_player.play("fade_out")
---
+++ platformer/src/UserInterface/UserInterface.gd
@@ -0,0 +1,35 @@
+extends Control
+
+onready var scene_tree: SceneTree = get_tree()
+onready var pause_overlay: ColorRect = $PauseOverlay
+onready var score_label: Label = $ScoreLabel
+onready var pause_title: Label = $PauseOverlay/Title
+
+var paused: = false setget set_paused
+
+
+func _ready() -> void:
+	PlayerData.connect("updated", self, "update_interface")
+	PlayerData.connect("died", self, "_PlayerData_player_died")
+	
+	update_interface()
+
+
+func _PlayerData_player_died() -> void:
+	self.paused = true
+	pause_title.text = "You died!"
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		self.paused = not self.paused
+		scene_tree.set_input_as_handled()
+
+func update_interface() -> void:
+	score_label.text = "Score: %s" % PlayerData.score
+
+func set_paused(value: bool) -> void:
+	paused = value
+
+	scene_tree.paused = value
+	pause_overlay.visible = value
